bfeb30851c001e75cb69e1410a2ceddf3063fc75
Boon Kit <gbk1386@gmail.com>
2020-12-24 11:22:06 +0000

    Create TicTacToe class and WinningConditions module.

--- main.rb
+++ main.rb
@@ -23,7 +23,7 @@ end
 
 class Player
   attr_reader :sign
-  attr_accessor :name :id
+  attr_accessor :name, :id
   @@id = 0
 
   def initialize(name, sign)
@@ -31,6 +31,48 @@ class Player
     @sign = sign
     @id = @@id
     @@id += 1
+  end
+end
+
+module WinningConditions
+  player1_win = false
+  player2_win = false
+
+  def horizontal(board, player1, player2)
+    # board.any row.all?(same sign)
+    board.each do |row|
+      player1_win = true if row.all?(player1.sign)
+      player2_win = true if row.all?(player2.sign)
+    end
+  end
+
+  def vertical(board, player1, player2)
+    board.transpose.each do |column|
+      player1_win = true if column.all?(player1.sign)
+      player2_win = true if column.all?(player2.sign)
+  end
+
+  def diagonal(board, player1, player2)
+    diagonal = true if board[0][0] == board[1][1] == board[2][2]
+    diagonal = true if board[0][2] == board[1][1] == board[2][0]
+    player1_win = true if diagonal && board[1][1] == player1.sign
+    player2_win = true if diagonal && board[1][1] == player2.sign
+end
+
+class TicTacToe
+  include WinningConditions
+
+  def initialize(player1 = Player.new('Player 1', 'O'), player2 = Player.new('Player 2', 'X'))
+    @player1 = player1
+    @player2 = player2
+    @board = Board.new()
+  end
+
+  def game()
+    # while not winning conditions && none?(" ")
+    # receive input from players
+    # turn by turn, display current board with signs on it
+
   end
 end
 game = Board.new()
